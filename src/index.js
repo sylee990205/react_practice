fb92a8f8fa5aa9970006b23dfd7c0cc155031104
sylee.990205@gmail.com <sylee.990205@gmail.com>
2023-09-26 11:01:48 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -4,12 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Library from "./chapter_03/Library";
+import Clock from "./chapter_04/Clock";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+setInterval(() => {
+    root.render(
+        <React.StrictMode>
+            <Clock />
+        </React.StrictMode>
+    )
+}, 1000);
+
+/*const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Library />
   </React.StrictMode>
-);
+);*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
