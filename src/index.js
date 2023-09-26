75bf0da1857ad390640cb38657ecaf263cd4ccc0
sylee.990205@gmail.com <sylee.990205@gmail.com>
2023-09-26 17:02:19 +0900

    Chapter08: 클릭 이벤트 처리하기

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import Library from "./chapter_03/Library";
-import Clock from "./chapter_04/Clock";
-import CommentList from "./chapter_05/CommentList";
-import NotificationList from "./chapter_06/NotificationList";
-import Counter from "./chapter_07/Counter";
-import MeasureExample from "./chapter_07/MeasureExample";
 import Accommodate from "./chapter_07/Accommodate";
+import ConfirmButton from "./chpater_08/ConfirmButton";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 /*
@@ -25,7 +19,7 @@ setInterval(() => {
 
 root.render(
     <React.StrictMode>
-        <Accommodate/>
+        <ConfirmButton/>
     </React.StrictMode>
 );
 
